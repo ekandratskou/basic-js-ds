b32de0f65e98126a577038c0a56eb1cba28263ca
Eduard <edyard2002@mail.ru>
2025-11-07 00:48:59 +0300

    feat: add the solution of task queue

--- src/queue.js
+++ src/queue.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib/errors');
+const { NotImplementedError } = require("../lib/errors");
 // const { ListNode } = require('../extensions/list-node.js');
 
 /**
@@ -12,23 +12,52 @@ const { NotImplementedError } = require('../lib/errors');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+
+class ListNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
 class Queue {
+  constructor() {
+    this.head = null; // указатель на первый элемент (начало очереди)
+    this.tail = null; // второй элемент (конец очереди)
+  }
+
   getUnderlyingList() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    const toPlain = (node) =>
+      node ? { value: node.value, next: toPlain(node.next) } : null; // для пустой очереди вернет null
+    return toPlain(this.head);
   }
 
-  enqueue(/* value */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  // добавляем элемент в конец очереди
+  enqueue(value) {
+    const newNode = new ListNode(value);
+    if (!this.head) {
+      this.head = newNode; // очередь пуста
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
   }
 
+  // удаляем элемент из начала очереди и возвращаем его значение
   dequeue() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    if (!this.head) {
+      return undefined; // очередь пуста
+    }
+    const removedValue = this.head.value;
+    this.head = this.head.next;
+    if (!this.head) {
+      this.tail = null; // если очередь опустела
+    }
+    return removedValue;
   }
 }
 
 module.exports = {
-  Queue
+  Queue,
 };
